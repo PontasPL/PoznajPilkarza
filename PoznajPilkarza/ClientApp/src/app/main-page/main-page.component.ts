0799d9d1f74952a7c5f681343114a3cb56a743a2
Unknown <pontas15@gmail.com>
2019-01-16 15:19:42 +0100

    Added shared folder
    Added shared folder for size resolution

--- PoznajPilkarza/ClientApp/src/app/main-page/main-page.component.ts
+++ PoznajPilkarza/ClientApp/src/app/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
