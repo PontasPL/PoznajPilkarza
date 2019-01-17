4713b98deafafb8330b0af9ecaee736f6a83f379
Unknown <pontas15@gmail.com>
2019-01-17 21:35:53 +0100

    Added scss folder with styles
    RWD fixed for 3 version and optimised.

--- PoznajPilkarza/ClientApp/src/app/main-page/main-page.component.ts
+++ PoznajPilkarza/ClientApp/src/app/main-page/main-page.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit, HostListener, ElementRef, Renderer2 } from '@angular
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
-
+  // checkMobile = false;
 
   constructor() { }
 
   ngOnInit() {
+    // this.ResizeSearchText();
   }
+
+  // public ResizeSearchText() {
+  //   if (window.innerWidth < 601) {
+  //     this.checkMobile = true;
+  //   }
+  //   console.log(window.innerWidth);
+  // }
 }
