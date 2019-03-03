0b904487b0163f1fe9b06601d53115c6a02a5f80
Patryk Pątek <pontas15@gmail.com>
2019-03-03 19:32:17 +0100

    Added Database migrations

---
+++ PoznajPilkarza/Migrations/20190303125756_InitialNationalites.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PoznajPilkarza.Migrations
+{
+    public partial class InitialNationalites : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Nationalities",
+                columns: table => new
+                {
+                    NationalityID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    CodeCountryTwoChars = table.Column<string>(nullable: true),
+                    CodeCountryThreeChars = table.Column<string>(nullable: true),
+                    TotalPopulation = table.Column<long>(nullable: false),
+                    PngImage = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    WikiLink = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nationalities", x => x.NationalityID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Nationalities");
+        }
+    }
+}
