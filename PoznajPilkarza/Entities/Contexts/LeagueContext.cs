882ec1c9b411936fd115485b269376d99daa92e9
Patryk Pątek <pontas15@gmail.com>
2019-03-03 23:23:01 +0100

    Added League context and migrations
    Added league context and initial migrations

---
+++ PoznajPilkarza/Entities/Contexts/LeagueContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PoznajPilkarza.Enitites;
+
+namespace PoznajPilkarza.Entities.Contexts
+{
+    public class LeagueContext :DbContext
+    {
+        public DbSet<Nationality> Nationalities { get; set; }
+        public DbSet<League> Leagues { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<League>().Property(b => b.Name).HasMaxLength(200).IsRequired();
+            modelBuilder.Entity<League>().Property(b => b.SeasonYear).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<League>().Property(b => b.Description).HasMaxLength(1000);
+            modelBuilder.Entity<League>().Property(b => b.WikiLink).HasMaxLength(300);
+        }
+
+        public LeagueContext(DbContextOptions<LeagueContext> options) : base(options)
+        {
+        }
+
+    }
+}
---
+++ PoznajPilkarza/Migrations/League/20190303203217_InitialLeagues.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PoznajPilkarza.Migrations.League
+{
+    public partial class InitialLeagues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Leagues",
+                columns: table => new
+                {
+                    LeagueId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy",
+                            SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 200, nullable: false),
+                    NationalityID = table.Column<int>(nullable: false),
+                    SeasonYear = table.Column<string>(maxLength: 100, nullable: false),
+                    WikiLink = table.Column<string>(maxLength: 300, nullable: true),
+                    Description = table.Column<string>(maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leagues", x => x.LeagueId);
+                    table.ForeignKey(
+                        name: "FK_Leagues_Nationalities_NationalityID",
+                        column: x => x.NationalityID,
+                        principalTable: "Nationalities",
+                        principalColumn: "NationalityID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Leagues_NationalityID",
+                table: "Leagues",
+                column: "NationalityID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Leagues");
+
+        }
+    }
+}
