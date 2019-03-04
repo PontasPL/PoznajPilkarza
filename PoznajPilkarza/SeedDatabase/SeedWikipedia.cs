3cbf5d23b239daffd452f6bf3a635549dacebe9b
Unknown <pontas15@gmail.com>
2019-03-04 12:57:19 +0100

    Static class for generate data from Wikipedia

---
+++ PoznajPilkarza/SeedDatabase/SeedWikipedia.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using PoznajPilkarza.Models;
+
+namespace PoznajPilkarza.SeedDatabase
+{
+    public static class SeedWikipedia 
+    {
+        public static async Task<WikipediaData> GetWiki(string searchQuery)
+        {
+            var wiki = new WikipediaData();
+            using (var client = new HttpClient())
+            {
+                var stringJson =
+                    await client.GetStringAsync(
+                        $@"https://en.wikipedia.org/w/api.php?action=opensearch&search={searchQuery}&limit=1&namespace=0&format=json");
+                JToken token = JToken.Parse(stringJson);
+                string patternDesc = $@"\([^()]*\)";
+                string patternBrackets = $@"[\[\]']";
+                wiki.Description = Regex.Replace(
+                    Regex.Replace(
+                        Regex.Replace(token[2].ToString(), patternDesc, ""),
+                        patternDesc, ""),
+                    patternBrackets, "");
+                wiki.Link = Regex.Replace(token[3].ToString(), patternBrackets, "");
+
+            }
+
+            return wiki;
+        }
+    }
+}
