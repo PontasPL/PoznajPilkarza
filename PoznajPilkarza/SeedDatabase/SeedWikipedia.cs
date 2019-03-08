65fc967374c9da12b68437ff16230da449357764
Unknown <pontas15@gmail.com>
2019-03-08 03:03:55 +0100

    Added try catch to function getWikipedia

--- PoznajPilkarza/SeedDatabase/SeedWikipedia.cs
+++ PoznajPilkarza/SeedDatabase/SeedWikipedia.cs
@@ -17,19 +17,28 @@ namespace PoznajPilkarza.SeedDatabase
             var wiki = new WikipediaData();
             using (var client = new HttpClient())
             {
-                var stringJson =
-                    await client.GetStringAsync(
-                        $@"https://en.wikipedia.org/w/api.php?action=opensearch&search={searchQuery}&limit=1&namespace=0&format=json");
-                JToken token = JToken.Parse(stringJson);
-                const string quote = "\"";
-                string patternDesc = $@"\([^()]*\)|\r\n|{quote}|\\";
-                string patternBrackets = $@"[\[\]']|\r\n|{quote}|\\";
-                wiki.Description = Regex.Replace(
-                    Regex.Replace(
-                        Regex.Replace(HtmlEntity.DeEntitize(token[2].ToString()), patternDesc, ""),
-                        patternDesc, "").Trim(),
-                    patternBrackets, "");
-                wiki.Link = Regex.Replace(token[3].ToString(), patternBrackets, "").Trim();
+                try
+                {
+                    var stringJson =
+                        await client.GetStringAsync(
+                            $@"https://en.wikipedia.org/w/api.php?action=opensearch&search={searchQuery}&limit=1&namespace=0&format=json");
+                    JToken token = JToken.Parse(stringJson);
+                    const string quote = "\"";
+                    string patternDesc = $@"\([^()]*\)|\r\n|{quote}|\\";
+                    string patternBrackets = $@"[\[\]']|\r\n|{quote}|\\";
+                    wiki.Description = Regex.Replace(
+                        Regex.Replace(
+                            Regex.Replace(HtmlEntity.DeEntitize(token[2].ToString()), patternDesc, ""),
+                            patternDesc, "").Trim(),
+                        patternBrackets, "");
+                    wiki.Link = Regex.Replace(token[3].ToString(), patternBrackets, "").Trim();
+                }
+                catch
+                {
+                    wiki.Description = "";
+                    wiki.Link = "";
+                }
+                
 
             }
 
